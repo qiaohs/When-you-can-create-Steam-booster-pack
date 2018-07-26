f453b04e74723b2eee44b507d23320ff0a44107e
BM_DY_AS_Lab <365960051@qq.com>
2018-07-26 18:00:00 +0800

    Update app.user.js

--- app.user.js
+++ app.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			When you can create Steam booster pack
 // @name:zh-CN			Steam补充包制作时间显示
-// @version	 		0.1
+// @version	 		0.2
 // @description			Show the time when you can create Steam booster pack at creator page.
 // @description:zh-CN		Steam已隐藏下一个补充包制作时间的显示，安装后可恢复显示
 // @author			QHS
@@ -10,13 +10,16 @@
 // @namespace 			https://greasyfork.org/users/155548-黑山東雲光圈研究所
 // @namespace 			https://steamcommunity.com/profiles/76561198132556503
 // ==/UserScript==
-$J('.booster_goo_cost').after('<div id="atime_insert" style="color: #f56795;margin-bottom: 12px;"></div>');
+
+$J('.booster_game_selector').after('<div id="atime_insert" style="color: #f56795;margin-bottom: 12px;"></div>');
 $J( '#booster_game_selector' ).change( function() {
-    check();
+    check();console.log(1);
 });
-$J(function check(){
-	var atime = CBoosterCreatorPage.sm_rgBoosterData[$J( '#booster_game_selector' ).val()].available_at_time;
-	if(atime){
-		$J('#atime_insert').html('You will not be able to create another Booster Pack until '+atime+'.');
+function check(){
+	if ($J('#booster_game_selector_booster')[0].children.length > 0) {
+		var atime = CBoosterCreatorPage.sm_rgBoosterData[$J( '#booster_game_selector' ).val()].available_at_time;
+		if(atime){
+			$J('#atime_insert').html('You will not be able to create another Booster Pack until '+atime+'.');
+		}
 	}
-})();
+}check();
