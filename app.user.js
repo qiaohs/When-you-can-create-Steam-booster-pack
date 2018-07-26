335c55c0f1804bb1e5aa7965d038251bd4064425
BM_DY_AS_Lab <365960051@qq.com>
2018-07-26 18:06:27 +0800

    Update app.user.js

--- app.user.js
+++ app.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			When you can create Steam booster pack
 // @name:zh-CN			Steam补充包制作时间显示
-// @version	 		0.2
+// @version	 		0.3
 // @description			Show the time when you can create Steam booster pack at creator page.
 // @description:zh-CN		Steam已隐藏下一个补充包制作时间的显示，安装后可恢复显示
 // @author			QHS
@@ -13,13 +13,15 @@
 
 $J('.booster_game_selector').after('<div id="atime_insert" style="color: #f56795;margin-bottom: 12px;"></div>');
 $J( '#booster_game_selector' ).change( function() {
-    check();console.log(1);
+    check();
 });
 function check(){
 	if ($J('#booster_game_selector_booster')[0].children.length > 0) {
 		var atime = CBoosterCreatorPage.sm_rgBoosterData[$J( '#booster_game_selector' ).val()].available_at_time;
 		if(atime){
 			$J('#atime_insert').html('You will not be able to create another Booster Pack until '+atime+'.');
-		}
+		}else{
+			$J('#atime_insert').html('');
+        }
 	}
 }check();
